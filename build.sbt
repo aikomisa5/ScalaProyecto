5738f9c2a0e6ef352c0937f89b39eed65ef16008
Misael <misacinco@gmail.com>
2019-03-28 13:16:41 -0300

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "ScalaProyecto"
+
+version := "1.0"
+
+scalaVersion := "2.12.8"
+    
---
+++ src/main/scala/Hello.scala
@@ -0,0 +1,7 @@
+/**
+  * Created by mbritos on 19/03/2019.
+  */
+
+object Hello extends App {
+  println("Hello, World!")
+}
